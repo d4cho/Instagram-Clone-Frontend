79b4d693d2ef5ca6cea9865b6858a895c16038a8
Daniel Cho <you@example.com>
2022-01-17 09:24:44 -0500

    create a new post modal started

---
+++ src/components/views/CreateNewPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateNewPost = () => {
+    return (
+        <div>
+            <div>create new post</div>
+        </div>
+    );
+};
+
+export default CreateNewPost;
